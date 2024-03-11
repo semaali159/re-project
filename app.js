a305d009d1e76f095dc3bdf838183ee63b87f241
semaali159 <161529190+semaali159@users.noreply.github.com>
2024-03-11 01:57:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const connectToDB = require("./config/db");
+const connectToDB = require("./conf/db");
 const cors = require("cors");
 //connection to database
 connectToDB();
