0ba3dccba8a9570de7733943bec1b3185dd5684b
semaali369 <semaali3699@gmail.com>
2024-03-30 06:50:51 -0700

    v8

--- controllers/pushNotifiController.js
+++ controllers/pushNotifiController.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const fcm = require("fcm-node");
-const serviceAccount = process.env.CREDENTIALS;
+const serviceAccount = require("../test.json");
 const asynchandler = require("express-async-handler");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -11,9 +11,7 @@ admin.initializeApp({
 
 // var FCM = new fcm(serviceAccount.private_key);
 sendPushNotification = asynchandler(async (req, res) => {
-  const registrationToken =
-    "d8jE08EsSaqGOIyR4tp0W-:APA91bEk1zqS4kzSVLk7vKk8uMRd1dc3KtvH0fW2DErlnUgLxqBWxsjdGN72YJUfPZ_0r2W2VHnvcgHd3pR31taP7SHQHxb-ob4OSLASmtPZeBIjorJmy5t1zKQ1WnqvuyucDaSy6moG";
-
+  const registrationToken = req.body.token;
   const message = {
     data: {
       score: "850",
