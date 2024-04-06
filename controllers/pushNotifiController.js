013ee3d8d99bdade2777240be821031834f8a70a
semaali369 <semaali3699@gmail.com>
2024-04-06 15:13:58 -0700

    v20

--- controllers/pushNotifiController.js
+++ controllers/pushNotifiController.js
@@ -1,59 +1,68 @@
 const admin = require("firebase-admin");
 const asynchandler = require("express-async-handler");
 const cron = require("node-cron");
+const schedule = require("node-schedule");
 const { Medicin } = require("../model/medicine");
-
 const serviceAccount = require("../test.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   projectID: "testt - 24942",
 });
-const messaging = admin.messaging();
 
-const sendPushNotification = asynchandler(async (req, res) => {
-  const registrationToken = req.body.token;
-  const medicin = await Medicin.findById(req.params.id);
-  const flag = medicin.EnableNotification;
+const sendPushNotification = async (req, res) => {
+  try {
+    const registrationToken = req.body.token;
+    const medicin = await Medicin.findById(req.params.id);
+    console.log(medicin.EnableNotification);
+    const flag = medicin.EnableNotification;
+    const messaging = admin.messaging();
 
-  console.log(flag);
-  if (flag) {
-    const message = {
-      notification: {
-        title: "اشعاراااات  حبييييب",
-        body: "اشعار من سمى حوول",
-      },
-      data: {
-        score: "850",
-        time: "2:45",
-      },
-      token: registrationToken,
-    };
-    // `0 */${repeat} * * *`
-    const repeat = 24 / medicin.repeat;
-    console.log(repeat);
-    cron.schedule(
-      `0 */${repeat} * * *`,
-      async () => {
-        console.log(repeat);
-        console.log("Running a job at 01:00 at America/Sao_Paulo timezone");
+    console.log(flag);
+    if (flag) {
+      const message = {
+        notification: {
+          title: "اشعاراااات  حبييييب",
+          body: "اشعار من سمى حوول",
+        },
+        data: {
+          score: "850",
+          time: "2:45",
+        },
+        token: registrationToken,
+      };
+      // `0 */${repeat} * * *`
+      const repeat = 24 / medicin.repeat;
+      console.log(repeat);
+      cron.schedule(
+        `0 */${repeat} * * *`,
+        async () => {
+          console.log(repeat);
+          console.log("Running a job at 01:00 at America/Sao_Paulo timezone");
 
-        await messaging
-          .send(message)
-          .then((response) => {
-            // Response is a message ID string.
-            console.log("Successfully sent message:", response);
-            return res.status(200).json({ message: "successfully", response });
-          })
-          .catch((error) => {
-            console.log("Error sending message:", error);
-          });
-      },
-      {
-        scheduled: true,
-        timezone: "America/Sao_Paulo",
-      }
-    );
+          await messaging
+            .send(message)
+            .then((response) => {
+              // Response is a message ID string.
+              console.log("Successfully sent message:", response);
+              return res
+                .status(200)
+                .json({ message: "successfully", response });
+            })
+            .catch((error) => {
+              console.log("Error sending message:", error);
+            });
+        },
+        {
+          scheduled: true,
+          timezone: "America/Sao_Paulo",
+        }
+      );
+    }
+    return res.status(200).json({ message: "sucess" });
+  } catch (e) {
+    return res.status(400).json({ message: e.message, success: false });
   }
-});
+};
 
 module.exports = sendPushNotification;
